9d107e17fce562a5187db5e6bd487a2cbeff9974
Kuroi Kyu <hello@kuroikyu.com>
2025-01-22 19:39:09 +0100

    docs: add description to existing keymaps

--- lua/kuroi/init.lua
+++ lua/kuroi/init.lua
@@ -8,11 +8,11 @@ wk.add { '<leader>p', group = '[P]roject' }
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
 
 -- Center screen after half page jumps
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump down half a screen and centre view' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump up half a screen and centre view' })
 
 -- Move current selection up or down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Visual - Move line down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Visual - Move line up' })
 
 vim.keymap.set('n', '<leader>pf', builtin.git_files, { desc = '[P]roject Git [F]iles' })
