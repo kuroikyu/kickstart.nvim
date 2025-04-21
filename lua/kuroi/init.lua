63d804854bdbf7e49253fc1a45e2544c7ee79712
Kuroi Kyu <hello@kuroikyu.com>
2025-04-21 19:22:45 +0200

    chore: misspelling

--- lua/kuroi/init.lua
+++ lua/kuroi/init.lua
@@ -10,8 +10,8 @@ wk.add { '<leader>p', group = '[P]roject' }
 vim.keymap.set('n', '<leader>pv', '<CMD>Oil --float --preview<CR>', { desc = '[P]roject [V]iew' })
 
 -- Center screen after half page jumps
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump down half a screen and centre view' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump up half a screen and centre view' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump down half a screen and center view' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump up half a screen and center view' })
 
 -- Move current selection up or down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Visual - Move line down' })
