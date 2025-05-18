96294ff092a022d367d9120051fb8677fbddfbec
Kuroi Kyu <hello@kuroikyu.com>
2025-05-18 20:48:09 +0200

    chore: add icons to whichkey keybinds

--- lua/kuroi/init.lua
+++ lua/kuroi/init.lua
@@ -1,24 +1,26 @@
 local wk = require 'which-key'
 local builtin = require 'telescope.builtin'
 
--- Groups
-wk.add { '<leader>p', group = '[P]roject' }
+wk.add {
+  -- Groups
+  { '<leader>p', group = '[P]roject', icon = '󰉋' },
 
--- Open project view
--- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
--- Oil.nvim variant
-vim.keymap.set('n', '<leader>pv', '<CMD>Oil --float --preview<CR>', { desc = '[P]roject [V]iew' })
+  -- Search files applying .gitignore
+  { '<leader>pf', builtin.git_files, desc = '[P]roject Git [F]iles' },
 
--- Center screen after half page jumps
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump down half a screen and center view' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump up half a screen and center view' })
+  -- Open project view
+  -- { '<leader>pv', vim.cmd.Ex, desc = '[P]roject [V]iew' },
+  -- Oil.nvim variant
+  { '<leader>pv', '<CMD>Oil --float --preview<CR>', desc = '[P]roject [V]iew (oil.nvim)', icon = '' },
 
--- Move current selection up or down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Visual - Move line down' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Visual - Move line up' })
+  -- Center screen after half page jumps
+  { '<C-d>', '<C-d>zz', desc = 'Jump down half a screen and center view', icon = '' },
+  { '<C-u>', '<C-u>zz', desc = 'Jump up half a screen and center view', icon = '' },
 
--- Search files applying .gitignore
-vim.keymap.set('n', '<leader>pf', builtin.git_files, { desc = '[P]roject Git [F]iles' })
+  -- Move visually lines selected up or down
+  { mode = 'v', 'J', ":m '>+1<CR>gv=gv", desc = 'Move line down', icon = '󰶡' },
+  { mode = 'v', 'K', ":m '<-2<CR>gv=gv", desc = 'Move line up', icon = '󰶣' },
+}
 
 -- Set indentation
 vim.opt.tabstop = 2
